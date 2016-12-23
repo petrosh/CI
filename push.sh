383874218d99f4490d4935705fc6899f6b031b2d
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-23 23:35:56 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,9 +1,11 @@
-git log -p -2 --pretty=format:"%h - %an, %ar : %s"
+git log -p -2 --pretty=format:"%h - %an or %cn, %ar : %s"
+echo name-only
 git log -p -2 --name-only
+echo name-status
+git log -p -1 --name-status
 echo GITHUB_URL ${GITHUB_URL:-`git config remote.origin.url`}
 echo REPO ${REPO}
 echo branch $TRAVIS_BRANCH
 echo pr $TRAVIS_PULL_REQUEST
 SHA=`git rev-parse --verify HEAD`
-echo $SHA
-
+echo SHA $SHA
