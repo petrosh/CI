90c0277b63f90918ef26460258ef53dc55e37183
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-23 23:06:09 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,2 +1,5 @@
 git log
-echo ${GITHUB_URL:-`git config remote.origin.url`}
+echo GITHUB_URL ${GITHUB_URL:-`git config remote.origin.url`}
+echo REPO ${REPO/https:\/\/github.com\//git@github.com:}
+echo branch $TRAVIS_BRANCH
+echo pr $TRAVIS_PULL_REQUEST
