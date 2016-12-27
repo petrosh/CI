dfdf9e8b4786a39d18fb262fc10a60ee76df71da
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-27 16:18:51 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -4,8 +4,12 @@ echo $REPO
 # owner target repo
 USR=`git remote show origin -n | grep h.URL | sed 's/.*\/\/github.com\///;s/.git$//'| cut -d'/' -f1`
 echo $USR
-INDIRI = 'https://api.github.com/repos/${USR}/${REPO}/pulls/${TRAVIS_PULL_REQUEST}'
-echo url $INDIRI
+I1="https://api.github.com/repos/"
+echo I1 $I1
+I2=$I1$URL/$REPO/pulls
+echo I2 $I2
+#INDIRI = 'https://api.github.com/repos/${USR}/${REPO}/pulls/${TRAVIS_PULL_REQUEST}'
+#echo url $INDIRI
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "Skipping deploy; just doing a build."
     git fetch && git diff --name-only ..origin
