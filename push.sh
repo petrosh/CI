a6d9d2620c487aead5eeb01733eb9b923ef4abbf
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-27 17:31:04 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -7,8 +7,8 @@ echo $USR
 I1="https://api.github.com/repos/"
 I2=${I1}${USR}/${REPO}/pulls
 echo $I2
-output = $(curl $I2 | sed -n 's/"user": "\([^"]*\)"/\1/p')
-echo output
+output = $(curl "$I2" | sed -n 's/"user": "\([^"]*\)"/\1/p')
+echo "$output"
 #INDIRI = ${I1}${USR}/${REPO}/pulls/${TRAVIS_PULL_REQUEST}
 #echo url $INDIRI
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
@@ -20,8 +20,8 @@ fi
 printf "**START**\n"
 I3=${TRAVIS_PULL_REQUEST}
 I4=$I2/$I3
-output = $(curl $I4 | sed -n 's/"user": "\([^"]*\)"/\1/p')
-echo output
+output = $(curl "$I4" | sed -n 's/"user": "\([^"]*\)"/\1/p')
+echo "$output"
 # fetch and diff
 git fetch && git diff --name-only ..origin
 printf "\n**END**"
