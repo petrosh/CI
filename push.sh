686aea27d154f65db117e94aabfcbcb9d54bc3c2
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-27 16:39:07 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -7,9 +7,9 @@ echo $USR
 I1="https://api.github.com/repos/"
 #echo I1 $I1
 #I2=${I1}${USR}/${REPO}/pulls
-#echo I2 $I2
-INDIRI = ${I1}${USR}/${REPO}/pulls/${TRAVIS_PULL_REQUEST}
-echo url $INDIRI
+echo I2:$I2
+#INDIRI = ${I1}${USR}/${REPO}/pulls/${TRAVIS_PULL_REQUEST}
+#echo url $INDIRI
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "Skipping deploy; just doing a build."
     git fetch && git diff --name-only ..origin
