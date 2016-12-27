4b58b0eb913ffbc44b6af7a12c811057a0c091e9
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-27 13:59:16 +0100

    Update before.sh

--- before.sh
+++ before.sh
@@ -1,7 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash -x
 
-echo TRIGGER_REPO $TRIGGER_REPO
-echo TRIGGER_COMMIT $TRIGGER_COMMIT
-cd `echo $TRIGGER_REPO | sed 's|[^/]*/||'` # Strip the user / org from the repo slug
-git fetch
-git checkout $TRIGGER_COMMIT
+if [ -n "$DEPENDENT_BUILD" ]; then
+  echo TRIGGER_REPO $TRIGGER_REPO
+  echo TRIGGER_COMMIT $TRIGGER_COMMIT
+  cd `echo $TRIGGER_REPO | sed 's|[^/]*/||'` # Strip the user / org from the repo slug
+  git fetch
+  git checkout $TRIGGER_COMMIT
+fi
