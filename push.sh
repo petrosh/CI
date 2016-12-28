833ee37948a11d621be9706d929f119cdd46d826
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-28 13:59:51 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,24 +1,12 @@
 #!/bin/bash -e
-REPO=$(basename `git rev-parse --show-toplevel`)
-echo $REPO
-# owner target repo
-USR=`git remote show origin -n | grep h.URL | sed 's/.*\/\/github.com\///;s/.git$//'| cut -d'/' -f1`
-echo $USR
+
 #echo travisslug
 #git config --local travis.slug
-echo ls-remote:
-git ls-remote --get-url
-git ls-remote --get-url origin
-echo basename
-basename $(git config --get remote.$(git config --get branch.master.remote).url) .git
-echo "-l"
-git config -l
-echo TRAVIS_REPO_SLUG $TRAVIS_REPO_SLUG
-echo mail
-LOCAL_BRANCH=`git name-rev --name-only HEAD`
-TRACKING_BRANCH=`git config branch.$LOCAL_BRANCH.merge`
-echo "$TRACKING_BRANCH"
-echo vv
+#echo ls-remote:
+#git ls-remote --get-url
+#git ls-remote --get-url origin
+#echo basename:
+#basename $(git config --get remote.$(git config --get branch.master.remote).url) .git
 #git config --get remote.github.url
 #I1="https://api.github.com/repos/"
 #I2=${I1}${USR}/${REPO}
@@ -29,15 +17,21 @@ echo vv
 #echo "$svaria"
 #INDIRI = ${I1}${USR}/${REPO}/pulls/${TRAVIS_PULL_REQUEST}
 #echo url $INDIRI
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo "Skipping deploy; just doing a build."
-    #git fetch && git diff --name-only ..origin
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    printf "**START**\n"
+    git fetch && git diff --name-only ..origin
+    REPO=$(basename `git rev-parse --show-toplevel`)
+    echo REPO: $REPO
+    echo TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG
+    # owner target repo
+    USR=`git remote show origin -n | grep h.URL | sed 's/.*\/\/github.com\///;s/.git$//'| cut -d'/' -f1`
+    echo USR: $USR
+    printf "\n**END**"
     exit 0
 fi
 # It is a pull request
-printf "**START**\n"
 git branch -vv
-git fetch origin refs/pull/$TRAVIS_PULL_REQUEST/head:sicuro
+#git fetch origin refs/pull/$TRAVIS_PULL_REQUEST/head:sicuro
 #I3=${TRAVIS_PULL_REQUEST}
 #I4=$I2/pulls/$I3
 #echo $I4
@@ -47,5 +41,3 @@ git fetch origin refs/pull/$TRAVIS_PULL_REQUEST/head:sicuro
 #output = $(curl "$I4" | sed -n 's/"user": "\([^"]*\)"/\1/p')
 #echo "$output"
 # fetch and diff
-git fetch && git diff --name-only ..origin
-printf "\n**END**"
